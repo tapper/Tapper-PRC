2f7564afc9359e65eb7fe1d595160ee20adcf324
Maik Hentsche <maik.hentsche@amd.com>
2009-04-20 11:55:16 +0200

    Artemis-PRC: integrated log_and_exec funtion, allows to mock system() in tests (e.g. to avoid reboot)

--- lib/Artemis/PRC.pm
+++ lib/Artemis/PRC.pm
@@ -34,6 +34,46 @@ BEGIN {
 	Log::Log4perl::init(Artemis::Config->subconfig->{files}{log4perl_cfg}); # ss5 2009-09-23
 }
 
+=head2 log_and_exec
+
+Execute a given command. Make sure the command is logged if requested and none
+of its output pollutes the console. In scalar context the function returns 0
+for success and the output of the command on error. In array context the
+function always return a list containing the return value of the command and
+the output of the command. (XXX: this function is also used in installer,
+think about refactoring it into a common module used by all Artemis projects)
+
+@param string - command
+
+@return success - 0
+@return error   - error string
+@returnlist success - (0, output)
+@returnlist error   - (return value of command, output)
+
+=cut
+
+sub log_and_exec
+{
+        my ($self, @cmd) = @_;
+        my $cmd = join " ",@cmd;
+        $self->log->debug( $cmd );
+        my $output=`$cmd 2>&1`;
+        my $retval=$?;
+        if (not defined($output)) {
+                $output = "Executing $cmd failed";
+                $retval = 1;
+        }
+        chomp $output if $output;
+        if ($retval) {
+                return ($retval >> 8, $output) if wantarray;
+                return $output;
+        }
+        return (0, $output) if wantarray;
+        return 0;
+}
+;
+
+
 
 
 =head2 mcp_send
